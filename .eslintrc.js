f936840eb83b104d7cc52dbb9c824a9f3fd5f4c0
mikaello <2505178+mikaello@users.noreply.github.com>
2019-10-15 20:39:01 +0200

    lint: add eslint for typescript and convert files to .tsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,23 @@
 module.exports = {
-    "extends": "airbnb",
-    "env": {
-        "browser": true,
-        "node": true
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  extends: [
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
     },
-    "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    }
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  settings: {
+    react: {
+      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
 };
